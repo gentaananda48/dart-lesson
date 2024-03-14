8869051b7c7952256cebfbc54cb946da39b63364
Genta Ananda Rakhsy <gentaananda48@gmail.com>
2024-03-14 22:03:52 +0700

    Update code add commenting

--- first.dart
+++ first.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+// This program prompts the user for their name and major, then prints a greeting message including their name and major.
 void main() {
-  stdout.write('Siapa Nama Anda: ');
+  stdout.write('Siapa nama anda: ');
   String name = stdin.readLineSync()!;
-  stdout.write('Apa Jurusan Anda: ');
+
+  stdout.write('Apa jurusan anda: ');
   String major = stdin.readLineSync()!;
+
   print('Hallo $name, dari jurusan $major');
 }
